4851a20ed6159b59c8ea88d694e157ae00fb3098
LarissaDanieli <profelarissadanieli@gmail.com>
2024-09-07 00:45:05 -0300

    Update main.java
    Add code

--- Fatorial/main.java
+++ Fatorial/main.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class Fatorial {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite um número inteiro positivo: ");
+
+        int numero = input.nextInt();
+        if (numero < 0) {
+            System.out.println("O número deve ser positivo.");
+        } else {
+            int fatorial = 1;
+            int i = 1;
+
+            while (i <= numero) {
+                fatorial *= i;
+                i++;
+            }
+            System.out.println("O fatorial de " + numero + " é " + fatorial + ".");
+        }
+
+        input.close();
+    }
+}
