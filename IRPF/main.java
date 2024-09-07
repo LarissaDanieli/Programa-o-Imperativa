69db5f0fc0d5555b0d770ee658b2eb176db64c98
LarissaDanieli <profelarissadanieli@gmail.com>
2024-09-07 00:46:51 -0300

    Update main.java
    Add code

--- IRPF/main.java
+++ IRPF/main.java
@@ -1,1 +1,42 @@
+import java.util.Scanner;
+
+public class IRPF {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite seu salário bruto: R$ ");
+        double salarioBruto = input.nextDouble();
+
+        double aliquota= 0.0;
+        double deducao = 0.0;
+
+        if (salarioBruto <=2259.20) {
+            aliquota = 0.0;
+            deducao = 0.0;
+
+        } else if (salarioBruto <=2826.65) {
+            aliquota = 0.075;
+            deducao = 169.44;
+
+        } else if (salarioBruto <=3751.05) {
+            aliquota = 0.15;
+            deducao = 381.44;
+
+        } else if (salarioBruto <= 4664.68){
+            aliquota = 0.225;
+            deducao = 662.77;
+
+        } else {
+            aliquota = 0.275;
+            deducao = 896.00;
+        }
+        double impostodeRenda = (salarioBruto * aliquota) - deducao;
+
+        double salarioLiquido = salarioBruto - impostodeRenda;
+
+        System.out.printf("Imposto de Renda: R$ %.2f%n", impostodeRenda);
+        System.out.printf("Salário líquido: R$ %.2f%n", salarioLiquido);
+
+input.close();
+        }
+}
 
