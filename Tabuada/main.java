7ecac8e32b3034df58cafd31d242071ba0788c6c
LarissaDanieli <profelarissadanieli@gmail.com>
2024-09-07 00:47:53 -0300

    Update main.java
    add code

--- Tabuada/main.java
+++ Tabuada/main.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = input.nextInt();
+
+        System.out.println("Tabuada de " + numero + ":");
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
+        }
+
+        input.close();
+    }
+}
 
